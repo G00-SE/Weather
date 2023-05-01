bb7cc3ba70fdc997b93a1504e2dbdf4d1d62c814
Serafim <soldatenkovserafim01@gmail.com>
2023-05-01 12:11:42 +0300

    Add venv Add Choose of language Add icons

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ class MainWindow(QWidget):
         self.place_name = QLineEdit(self)
         self.place_button = QPushButton(self)
         self.a = QLabel(self)
+        self.language_button_ru = QPushButton(self)
+        self.language_button_en = QPushButton(self)
 
-        #Ввод данных
+        #Создание Элементов
         self.place_label = QLabel('<h3 style="color: rgb(250, 255, 255);">Город:</h3>', self)
         self.temperature_label = QLabel('<h3 style="color: rgb(250, 255, 255);">Температура:</h3>', self)
         self.wind_label = QLabel('<h3 style="color: rgb(250, 255, 255);">Скорость ветра:</h3>', self)
@@ -42,7 +44,10 @@ class MainWindow(QWidget):
         self.detail_information = QLabel(self)
         self.time_information = QLabel(self)
 
+        #Подключение кнопок
         self.place_button.clicked.connect(self.get_information)
+        self.language_button_en.clicked.connect(self.set_language_en)
+        self.language_button_ru.clicked.connect(self.set_language_ru)
 
         self.init_ui()
 
@@ -53,11 +58,21 @@ class MainWindow(QWidget):
         self.place_button.setStyleSheet("background-color: rgba(3,155,228,0.9)")
         self.place_name.setStyleSheet("background-color: rgba(3,155,228,0.9)")
         self.place_button.setText("Поиск")
+        self.language_button_ru.setStyleSheet("background-color: rgba(3,155,228,0.9)")
+        self.language_button_en.setStyleSheet("background-color: rgba(3,155,228,0.9)")
+        self.language_button_ru.setText("Русский")
+        self.language_button_en.setText("English")
         self.resize(400, 500)
         self.setWindowTitle('Weather')
 
+        #Разметка
         vbox, hbox = QVBoxLayout(), QHBoxLayout()
 
+        hbox.addWidget(self.language_button_ru)
+        hbox.addWidget(self.language_button_en)
+        vbox.addLayout(hbox)
+
+        hbox = QHBoxLayout()
         hbox.addWidget(self.place_name)
         hbox.addWidget(self.place_button)
         vbox.addLayout(hbox)
@@ -108,6 +123,7 @@ class MainWindow(QWidget):
 
         self.setLayout(vbox)
 
+        #Получение информации
     def get_information(self):
         place = self.place_name.text()
         mgr = owm.weather_manager()
@@ -131,8 +147,7 @@ class MainWindow(QWidget):
         self.humidity_information.setText(str(humi) + "%")
         self.clouds_information.setText(str(cl) + "%")
         self.time_information.setText(ti)
-        print(st)
-        print(dt)
+
         if st == "Clouds" and dt == "overcast clouds":
             self.status_image.setPixmap(self.pixmap_clouds)
             self.status_information.setText("Облака")
@@ -165,6 +180,27 @@ class MainWindow(QWidget):
             self.status_image.setPixmap(self.pixmap_mist)
             self.status_information.setText("Туман")
             self.detail_information.setText("Густой туман")
+
+        #Перевод
+    def set_language_en(self):
+        self.place_label.setText('<h3 style="color: rgb(250, 255, 255);">York:<h3>')
+        self.temperature_label.setText('<h3 style="color: rgb(250, 255, 255);">temperature:<h3>')
+        self.wind_label.setText('<h3 style="color: rgb(250, 255, 255);">speed of wind:<h3>')
+        self.humidity_label.setText('<h3 style="color: rgb(250, 255, 255);">humidity:<h3>')
+        self.clouds_label.setText('<h3 style="color: rgb(250, 255, 255);">clouds:<h3>')
+        self.status_label.setText('<h3 style="color: rgb(250, 255, 255);">status:<h3>')
+        self.detail_label.setText('<h3 style="color: rgb(250, 255, 255);">detail:<h3>')
+        self.time_label.setText('<h3 style="color: rgb(250, 255, 255);">time:<h3>')
+
+    def set_language_ru(self):
+        self.place_label.setText('<h3 style="color: rgb(250, 255, 255);">Город:</h3>')
+        self.temperature_label.setText('<h3 style="color: rgb(250, 255, 255);">Температура:</h3>')
+        self.wind_label.setText('<h3 style="color: rgb(250, 255, 255);">Скорость ветра:</h3>')
+        self.humidity_label.setText('<h3 style="color: rgb(250, 255, 255);">Влажность:</h3>')
+        self.clouds_label.setText('<h3 style="color: rgb(250, 255, 255);">Облачность:</h3>')
+        self.status_label.setText('<h3 style="color: rgb(250, 255, 255);">Статус:</h3>')
+        self.detail_label.setText('<h3 style="color: rgb(250, 255, 255);">Детали:</h3>')
+        self.time_label.setText('<h3 style="color: rgb(250, 255, 255);">Время замера:</h3>')
 
 
 
